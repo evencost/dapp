8842e899b724649641d57fab4298d530a84c2ae4
Pylyp Radionov <me@mr13.tech>
2023-08-13 14:38:52 +0700

    fix nav.

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -2,16 +2,24 @@
 
 import { useRouter } from 'next/navigation'
 import { Button } from '@/components/Button'
+import { useWeb3Modal } from '@web3modal/react'
+import { useAccount } from 'wagmi'
 
 export const Landing = () => {
   const router = useRouter()
+
+  const { open } = useWeb3Modal()
+
+  const { isConnected } = useAccount()
+
+  if (isConnected) router.push('/create')
 
   return (
     <div className='flex flex-col items-center justify-center gap-6'>
       <p className='items- text-center text-5xl text-[#131635]'>
         Autoinvest decentralized way
       </p>
-      <Button label={'Create plan'} onClick={() => router.push('/create')} />
+      <Button label={'Create plan'} onClick={open} />
       <Button
         label={'To the dashboard'}
         onClick={() => router.push('/dashboard')}

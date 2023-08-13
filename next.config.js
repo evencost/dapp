71319941bc1fba1666e9165a1a20f077e91309d4
Pylyp Radionov <me@mr13.tech>
2023-08-13 22:31:51 +0700

    clean.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // env: {
-  // WC_PROJECT_ID: 'ef3cb63e5aa5d488235957499a4fe5cd',
-  // },
+  env: {
+    WC_PROJECT_ID: 'ef3cb63e5aa5d488235957499a4fe5cd',
+  },
   output: 'export',
 }
 
